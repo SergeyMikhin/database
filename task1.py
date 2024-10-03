8edad8771e1f6b923c73bb818b558421c9a68d71
SergeyMikhin <akvoliahim@gmail.com>
2024-10-03 20:24:50 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,19 @@
+#1st program
+print("1st program")
+print(9 ** 0.5 * 5 )
+#2nd program
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+print("3rd program")
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(6 == 8)
+#4th program
+print("4th program")
+print(1234 % 1000)
+print(234 // 10)
+print(5678)
+print(5678 % 1000)
+print(678 // 10)
+print(23 + 67)
